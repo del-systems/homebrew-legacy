032816c78a9007bade60831361c75c34eed025eb
Shakhzod Ikromov <aabbcc.double@gmail.com>
2024-08-07 19:41:08 +0300

    Add `mysql-client-legacy@5.7` (#1)

---
+++ Formula/mysql-client-legacy@5.7.rb
@@ -0,0 +1,44 @@
+class MysqlClientLegacyAT57 < Formula
+  desc "Open source relational database management system"
+  homepage "https://dev.mysql.com/doc/refman/5.7/en/"
+  url "https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-boost-5.7.42.tar.gz"
+  sha256 "7e1a7d45e7ca382eb3a992f63631c380904dd49c89f3382ec950aef01997524f"
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "openssl@1.1"
+  uses_from_macos "libedit"
+
+  def install
+    # https://bugs.mysql.com/bug.php?id=87348
+    # Fixes: "ADD_SUBDIRECTORY given source
+    # 'storage/ndb' which is not an existing"
+    inreplace "CMakeLists.txt", "ADD_SUBDIRECTORY(storage/ndb)", ""
+
+    # -DINSTALL_* are relative to `CMAKE_INSTALL_PREFIX` (`prefix`)
+    args = %W[
+      -DCOMPILATION_COMMENT=Homebrew
+      -DDEFAULT_CHARSET=utf8
+      -DDEFAULT_COLLATION=utf8_general_ci
+      -DINSTALL_DOCDIR=share/doc/#{name}
+      -DINSTALL_INCLUDEDIR=include/mysql
+      -DINSTALL_INFODIR=share/info
+      -DINSTALL_MANDIR=share/man
+      -DINSTALL_MYSQLSHAREDIR=share/mysql
+      -DWITH_BOOST=boost
+      -DWITH_EDITLINE=system
+      -DWITH_SSL=yes
+      -DWITH_UNIT_TESTS=OFF
+      -DWITHOUT_SERVER=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mysql --version")
+  end
+end
