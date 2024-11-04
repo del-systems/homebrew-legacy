4c8345de2a5cd03c402d9733f4cd23fed284e8c6
Shakhzod Ikromov <aabbcc.double@gmail.com>
2024-11-04 13:13:42 +0200

    Remove 404 mirrors

--- Formula/openssl-legacy@1.1.rb
+++ Formula/openssl-legacy@1.1.rb
@@ -2,11 +2,6 @@ class OpensslLegacyAT11 < Formula
   desc "Cryptography and SSL/TLS Toolkit"
   homepage "https://openssl.org/"
   url "https://www.openssl.org/source/openssl-1.1.1w.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.1w.tar.gz"
-  mirror "http://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.1.1w.tar.gz"
-  mirror "https://www.openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz"
-  mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz"
-  mirror "http://www.mirrorservice.org/sites/ftp.openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz"
   sha256 "cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d93925208b76ac8"
   license "OpenSSL"
   version_scheme 1
